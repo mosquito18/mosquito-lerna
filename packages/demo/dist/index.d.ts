0fd4e3c6ee8298e3d57ec52ac1fc9f91bff417e5
wenjielin <wenjielin@whale.imn>
2021-12-11 19:02:50 +0800

    init

--- packages/demo/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.less';
-interface demoProps {
-    msg: string;
-    onClick: () => void;
-}
-declare const Demo: React.FC<demoProps>;
-export default Demo;
--- packages/demo/dist/utils/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const createBEM: (prefixCls: string) => (className?: string) => string;
-export declare const t: (key: string, options?: object) => string;
